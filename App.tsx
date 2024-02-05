57b93a56f87cbdc64aae898993512f1ad7913bd3
bourkison <harrisonbourke97@gmail.com>
2024-02-05 18:20:00 +0000

    Added Home screen, Auth, and Geo

--- App.tsx
+++ App.tsx
@@ -1,16 +1,31 @@
+// import {Geo} from '@aws-amplify/geo';
 import {Amplify} from 'aws-amplify';
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 import amplifyconfig from './aws-exports';
+
+import Home from '@/screens/Home';
 
 Amplify.configure(amplifyconfig);
 
 export default function App() {
+    // useEffect(() => {
+    //     const fetchRes = async () => {
+    //         const results = await Geo.searchByText('99 Downs Park Road', {
+    //             countries: ['GBR'],
+    //             maxResults: 10,
+    //         }).catch(err => console.error(err));
+    //         console.log('results', results);
+    //     };
+
+    //     fetchRes();
+    // }, []);
+
     return (
         <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
+            <Home />
             <StatusBar style="auto" />
         </View>
     );
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mmitmCognito": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "geo": {
+    "mmitmplaceindex": {
+      "Arn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
